b9344971feeef929453aafc6675a1f2f5c8c82a9
Franco <Franco97222@gmail.com>
2023-05-31 11:08:27 -0300

    Agregue programa del arduino

---
+++ Arduino/arduino.ino
@@ -0,0 +1,32 @@
+
+const int Trigger = 4;   //Pin digital 2 para el Trigger del sensor
+const int Echo = 5;   //Pin digital 3 para el echo del sensor
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);//iniciailzamos la comunicación
+  pinMode(Trigger, OUTPUT); //pin como salida
+  pinMode(Echo, INPUT);  //pin como entrada
+  digitalWrite(Trigger, LOW);//Inicializamos el pin con 0
+  
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  long t; //timepo que demora en llegar el eco
+  long d; //distancia en centimetros
+
+  digitalWrite(Trigger, HIGH);
+  delayMicroseconds(10);          //Enviamos un pulso de 10us
+  digitalWrite(Trigger, LOW);
+  
+  t = pulseIn(Echo, HIGH); //obtenemos el ancho del pulso
+  d = t/59;             //escalamos el tiempo a una distancia en cm
+  
+  Serial.print("Distancia: ");
+  Serial.print(d);      //Enviamos serialmente el valor de la distancia
+  Serial.print("cm");
+  Serial.println();
+  delay(100);          //Hacemos una pausa de 100ms
+}
